583d2ad109a590f37527891b8c6b78fd6e0e9157
Cristhian Quoghi Ficher <103082050+CristhianFicher@users.noreply.github.com>
2025-09-01 22:52:30 +0000

    Css

--- site/src/App.jsx
+++ site/src/App.jsx
@@ -1,23 +1,49 @@
+import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import './App.css';
-import CadastroAlunos from './CadastroAlunos/cadastroAlunos';
+import CadastroAlunos from "./CadastroAlunos/cadastroAlunos";
 import CadastroEmpresas from "./CadastroEmpresas/cadastroEmpresas";
 import AvaliacaoExperiencia from "./AvaliacaoDesempenho/avaliacao";
 
 function App() {
+  const [isLoginOpen, setIsLoginOpen] = useState(false);
+
   return (
     <div className="app-container">
-  <h1>Bem-vindo!</h1>
-  <Link to="/cadastroAlunos">
-    <button className="btn">Cadastrar Alunos</button>
-  </Link>
-  <Link to="/cadastroEmpresas">
-    <button className="btn">Cadastrar Empresas</button>
-  </Link>
-  <Link to = "/avaliacao">
-    <button className="btn">Avaliação de Alunos</button>
-  </Link>
-</div>
+      <h1>Bem-vindo!</h1>
+
+      {/* Botão de Login */}
+      <button className="btn" onClick={() => setIsLoginOpen(true)}>
+        Login
+      </button>
+
+      {/* Modal de Login */}
+      {isLoginOpen && (
+        <div className="modal-overlay">
+          <div className="modal-container">
+            <h2>Login</h2>
+            <form>
+              <input type="text" placeholder="Usuário" required />
+              <input type="password" placeholder="Senha" required />
+              <button type="submit">Entrar</button>
+            </form>
+            <button className="close-btn" onClick={() => setIsLoginOpen(false)}>
+              X
+            </button>
+          </div>
+        </div>
+      )}
+
+      {/* Navegação */}
+      <Link to="/cadastroAlunos">
+        <button className="btn">Cadastrar Alunos</button>
+      </Link>
+      <Link to="/cadastroEmpresas">
+        <button className="btn">Cadastrar Empresas</button>
+      </Link>
+      <Link to="/avaliacao">
+        <button className="btn">Avaliação de Alunos</button>
+      </Link>
+    </div>
   );
 }
 
@@ -33,6 +59,5 @@ function Cadastro() {
     </Router>
   );
 }
-
 
 export default Cadastro;
